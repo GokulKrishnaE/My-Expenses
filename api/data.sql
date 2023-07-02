9d555575ecb5bdf92098b91ab1254893fc2ad023
Gokul Krishna E <gokul44krishna@gmail.com>
2023-07-02 15:37:20 +0530

    db changes

--- api/data.sql
+++ api/data.sql
@@ -6,13 +6,36 @@ CREATE TABLE EXPENSES(
     title VARCHAR(50),
     category VARCHAR(255),
     date VARCHAR(255),
-    amount INT
+    amount INT,
+    month VARCHAR(255),
+    year VARCHAR(255),
+    bookname VARCHAR(255)
+);
+
+
+CREATE TABLE EARNINGS(
+    id VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255),
+    title VARCHAR(50),
+    date VARCHAR(255),
+    amount INT,
+    month VARCHAR(255),
+    year VARCHAR(255),
+    bookname VARCHAR(255)
+);
+
+
+CREATE TABLE BOOKS(
+    id VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255),
+    bookname VARCHAR(50),
 );
 CREATE TABLE USERS(
     username VARCHAR(255),
     password VARCHAR(50),
     email VARCHAR(255) PRIMARY KEY,
-    isAdmin BOOLEAN
+    isAdmin BOOLEAN,
+    categories VARCHAR[]
 );
 
 INSERT INTO EXPENSES(id,email,title,category,date,amount) VALUES('0','gokulkrishna.e@gmail.com','first expense','travel','04-04-2023',200);
--- api/db.js
+++ api/db.js
@@ -2,11 +2,17 @@ import pg from 'pg'
 const Pool = pg.Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'Predator4@@',
-    host: 'localhost',
-    port: 5432,
-    database: 'postgres'
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:Predator4@@@localhost:5432/postgres',
+    ssl: process.env.DATABASE_URL ? true : false
 })
+
+// const pool = new Pool({
+//     // user: 'postgres',
+//     // password: 'Predator4@@',
+//     // host: 'localhost',
+//     // port: 5432,
+//     // database: 'postgres'
+//     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// })
 
 export default pool
