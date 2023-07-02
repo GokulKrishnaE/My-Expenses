7dd1a35fa6f95b1935c361bf2486890ca6ce9ca1
Gokul Krishna E <gokul44krishna@gmail.com>
2023-07-02 15:57:45 +0530

    folder name change

--- server/verification/verify.js
+++ server/verification/verify.js
@@ -3,7 +3,7 @@ import Jwt from 'jsonwebtoken'
 export const verifyTokenEmail = (req,res,next)=>{
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if(token==null) return res.send('your are not autherized')
+    if(token==null) return res.send('your are not autherized from me!')
     Jwt.verify(token,`${process.env.ACCESS_TOKEN_SECRET}`,(err,payload)=>{
         if(err) return res.send("token not valid")
         req.email = payload.email
@@ -13,7 +13,7 @@ export const verifyTokenEmail = (req,res,next)=>{
 export const verifyToken = (req,res,next)=>{
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if(token==null) return res.send('your are not autherized')
+    if(token==null) return res.send('your are not autherized from me!')
     Jwt.verify(token,`${process.env.ACCESS_TOKEN_SECRET}`,(err,payload)=>{
         if(err) return res.send('token not valid')
         next()
