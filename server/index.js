bc27d03626e171540d9da963badcd46f8866389a
Gokul Krishna E <68217660+GokulKrishnaE@users.noreply.github.com>
2024-05-30 19:14:02 +0530

    cors issue

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ const app = express()
 dotenv.config()
 
 
-app.use(cors({origin: 'https://my-expenses-server-15dcbldte-gokulkrishnaes-projects.vercel.app/'}))
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*")
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+next() 
+})
 app.use(cookieParser())
 app.use(express.json())
 
