2840d0a5db9a8520a414964ddd48d958ed50a022
Gokul Krishna E <68217660+GokulKrishnaE@users.noreply.github.com>
2024-05-30 19:17:26 +0530

    cors issue

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config()
 
 
 app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*")
+res.header("Access-Control-Allow-Origin", "https://my-expenses-client.vercel.app")
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 next() 
 })
